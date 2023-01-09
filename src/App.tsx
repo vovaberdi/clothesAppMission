85988f1b5aff1bbf7047df2e21ed9b878ff0a576
vovaberdi <vova.berdibek@gmail.com>
2023-01-10 01:46:40 +0200

    building redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 
+import { Provider } from 'react-redux';
 import './App.css';
 import Header from './Components/LayoutArea/Header/Header';
 import Main from './Components/LayoutArea/Main/Main';
+import store from './store/store';
 
 
 function App() {
   return (
     <div className="App">
+      <Provider store={store}>
          <header><Header/></header>
          <h1>My Clothes</h1>
          <main><Main/></main>
+         </Provider>
     </div>
   );
 }
