571ab113272f81d4135c52be3306e2c52d14c4a0
vovaberdi <vova.berdibek@gmail.com>
2023-01-13 22:59:10 +0200

    finshed super merge losash function on data for sets component

--- src/Components/Pages/mySets/Set.tsx
+++ src/Components/Pages/mySets/Set.tsx
@@ -1,53 +1,57 @@
-// import { Button, Card, CardBody, CardFooter, CardHeader, Heading, Text } from "@chakra-ui/react";
-import { MySet } from "../../../models/Types";
+import { Button, Card, CardBody, CardFooter, CardHeader, Heading, Text } from "@chakra-ui/react";
+import { useEffect } from "react";
 
-
-
-export const Sets: React.FC<{ objects: MySet[] }> = ({ objects }) => {
-  console.log("setCheack:", objects)
-  return (
-    <div>
-      {objects.map((object) => (
-        <div key={object.id}>
-          <h1>{object.brand}</h1>
-          <p>{object.type}</p>
-          <h1>{object.size}</h1>
-          <p>{object.color}</p>
-        </div>
-      ))}
-    </div>
-  );
+interface MyMerge {
+  pants_brand: string;
+  pants_color: string;
+  pants_id: number;
+  pants_selected: true
+  pants_size: number;
+  pants_timestamp: string;
+  pants_type?: string;
+  selected?: boolean;
+  shirt_brand: string;
+  shirt_color: string;
+  shirt_id: number;
+  shirt_selected?: boolean;
+  shirt_size: string;
+  shirt_timestamp: string;
+  shirt_type?: string;
+  shoes_brand: string;
+  shoes_color: string;
+  shoes_id: number;
+  shoes_selected?: boolean;
+  shoes_size: number;
+  shoes_timestamp: string;
+  shoes_type?: string;
 }
-// function Sets(props:MySet): JSX.Element {
 
 
-//   return (
-//     <div className="set">
+function MySets(props:MyMerge): JSX.Element {
 
-           
-//     </div>
-//   );
-// }
+  useEffect(()=>{
+    console.log("MySets:", props)
+  },[])
 
-// export default Sets;
+  return (
+    <div className="set">
 
-
-
-
-
-
-
-
-
-
-        {/* <Card>
+                  <Card>
                      <CardHeader>
-                     <Heading size='md'>{props.brand}{props.type}</Heading>
+                     <Heading size='md'>{props.shirt_brand}{props.shirt_size}{props.shirt_color}</Heading>
                    </CardHeader>
                     <CardBody>
-                     <Text>{props.color}{props.size}</Text>
+                     <Text>{props.pants_brand}{props.pants_size}{props.pants_color}</Text>
+                   </CardBody>
+                   <CardBody>
+                     <Text>{props.shoes_brand}{props.shoes_size}{props.shoes_color}</Text>
                    </CardBody>
                    <CardFooter>
                      <Button onClick={()=>window.localStorage.clear()}>View here</Button>
                    </CardFooter>
-                 </Card>  */}
+                 </Card>
+    </div>
+  );
+}
+
+export default MySets;
--- src/Components/Pages/mySets/setList.tsx
+++ src/Components/Pages/mySets/setList.tsx
@@ -2,53 +2,51 @@
 import { Box, SimpleGrid } from "@chakra-ui/react";
 import _ from "lodash";
 import { useEffect } from "react";
-import { MySet } from "../../../models/Types";
-import{Sets} from "./Set";
+import MySets from "./Set";
 import "./setList.css";
 
+interface MyMerge {
+  pants_brand: string;
+  pants_color: string;
+  pants_id: number;
+  pants_selected: true
+  pants_size: number;
+  pants_timestamp: string;
+  pants_type?: string;
+  selected?: boolean;
+  shirt_brand: string;
+  shirt_color: string;
+  shirt_id: number;
+  shirt_selected?: boolean;
+  shirt_size: string;
+  shirt_timestamp: string;
+  shirt_type?: string;
+  shoes_brand: string;
+  shoes_color: string;
+  shoes_id: number;
+  shoes_selected?: boolean;
+  shoes_size: number;
+  shoes_timestamp: string;
+  shoes_type?: string;
+}
+
 function SetList(): JSX.Element {
-  // const sets = useSelector((state: RootState) => state.sets);
-  // const clothes = useSelector((state: RootState) => state.clothes);
-  // const dispatch = useDispatch();
-
-  // function onSelect(id: number) {
-  //   dispatch({ type: 'SELECT_SET', id });
-  // }
-
-  // function onDeselect(id: number) {
-  //   dispatch({ type: 'DESELECT_SET', id });
-  // }
 
   useEffect(()=>{
     getSets()
     abb()
     check()
-    ale()
-    test.forEach((a) => Object.values(a).forEach((b) => (vova += b)));
-    console.log(vova);
-
+    console.log("merged:", merged)
   },[])
 
- const getSets = () => {
-     for (let i = 0, len = localStorage.length; i < len; i++) {
-        let myKey:any = localStorage.key(i);
-        let temp = localStorage.getItem(myKey) || '{}';
-        temp.length > 10 ?
-        storedSets.push(JSON.parse(temp)) :
-        console.log(temp);       
-  }
-}
-
-interface MyItem {
-  /* properties of your object*/
-}
-
-interface MyArray {
-  id: number;
-  brand: string;
-  type: string;
-  size: string;
-  color: string;
+const getSets = () => {
+        for (let i = 0, len = localStorage.length; i < len; i++) {
+           let myKey:any = localStorage.key(i);
+           let temp = localStorage.getItem(myKey) || '{}';
+           temp.length > 10 ?
+           storedSets.push(JSON.parse(temp)) :
+           console.log(temp);       
+       }
 }
   
 
@@ -65,24 +63,19 @@ interface MyArray {
       
 
     const check = () =>{
-         storedSets.map((item:any) =>{
-          item.map((item: any) =>{
-            test.push(item)
-          })
-         })
+         storedSets.map((item:any) =>{ superMerge(item[0], item[1], item[2]) })
     }
-    let test: any[] = []
-    let vova = "";
+    let merged: MyMerge[] = []
 
-    const ale = () =>{
-      let obj1 = {id: 1, type: 'shoes', color: 'black', size: 37, brand: 'Tommy Hilfiger'};
-      let obj2 = {id: 2, type: 'shirt', color: 'blue', size: 'M', brand: 'Polo'};
-      let obj3 = {id: 3, type: 'pants', color: 'green', size: '32', brand: 'Levi'};
+    const superMerge = (obj1:any, obj2:any, obj3:any) =>{
 
-      let mergedObj = Object.assign({}, obj1, obj2, obj3);
-        // mergedObj['item_id'] = mergedObj.id;
-        // delete mergedObj.id;
-         console.log("ale:",mergedObj)
+        let mergeWithCustomizer = function(objValue: any, srcValue: any, key: string, object: { [x: string]: any; }, source: { type: string; }) {
+          let type = source.type + "_";
+          let newKey = type + key;
+          object[newKey] = srcValue;
+        };
+        let mergedObj = _.mergeWith({}, obj1, obj2, obj3, mergeWithCustomizer);
+        merged.push(mergedObj);
     }
 
 
@@ -93,7 +86,7 @@ interface MyArray {
                 
                 <SimpleGrid  columns={{ sm: 2, md: 4 }} spacing='8' p='10' textAlign='center' rounded='lg' color='gray.400'>
 
-                {storedSets.map(item => <Sets objects={item} />)}
+                {merged.map((i) => <MySets pants_brand={i.pants_brand} pants_color={i.pants_color} pants_id={i.pants_id} pants_size={i.pants_size} pants_timestamp={i.pants_timestamp} shirt_brand={i.shirt_brand} shirt_color={i.shirt_color} shirt_id={i.shirt_id} shirt_size={i.shirt_size} shirt_timestamp={i.shirt_timestamp} shoes_brand={i.shoes_brand} shoes_color={i.shoes_color} shoes_id={i.shoes_id} shoes_size={i.shoes_size} shoes_timestamp={i.shoes_timestamp} pants_selected={true} />)}
                 
                 </SimpleGrid>
                 </Box> 
